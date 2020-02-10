2d954030ece63e8ec834237c2ddf2775b20618b8
Shrikant Kamble <shrikant.kamble@global.internal>
2020-02-10 04:38:15 -0800

    Filtering with dates.

--- src/app/hotels/hotels.component.ts
+++ src/app/hotels/hotels.component.ts
@@ -17,29 +17,41 @@ export class HotelsComponent implements OnInit {
   constructor(private _router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    
     // subscribe to the queryparam
     this.route.queryParams.subscribe(queryParams => {
-      // do something with the query params
-      let key = this.sortParam = (queryParams.sort) ? queryParams.sort : 'price';
-      this.filteredHotels = this.hotels;
-
-      if (queryParams.departing_from) {
-        this.filteredHotels = this.filteredHotels.filter(
-          hotel => hotel.departing_from.toLowerCase() === queryParams.departing_from.toLowerCase()
-        );
-      }
-
-      if (!key) return;
-
-      this.filteredHotels.sort((a, b) => {
-        if (a[key] < b[key]) return -1;
-        else if (a[key] > b[key]) return 1;
-        else return 0;
-      });
+      this.sortAndFilter(queryParams)
     });
   }
 
+  sortAndFilter = (queryParams) => {
+    let key = this.sortParam = (queryParams.sort) ? queryParams.sort : 'price';
+    this.filteredHotels = this.hotels;
+
+    if (queryParams.departing_from) {
+      this.filteredHotels = this.filteredHotels.filter(
+        hotel => hotel.departing_from.toLowerCase() === queryParams.departing_from.toLowerCase()
+      );
+    }
+
+    if (queryParams.start_date) {
+      let query_start_date = new Date(queryParams.start_date);
+      this.filteredHotels = this.filteredHotels.filter(
+        hotel => {
+          let time_1 = new Date(hotel.start_date); 
+          return time_1 > query_start_date
+        }
+      );
+    }
+
+    if (!key) return;
+
+    this.filteredHotels.sort((a, b) => {
+      if (a[key] < b[key]) return -1;
+      else if (a[key] > b[key]) return 1;
+      else return 0;
+    });
+  }
+  
   sort = (key: string) => {
     // set the query string in URL
     this._router.navigate(['/hotels'], {
@@ -61,8 +73,13 @@ export class HotelsComponent implements OnInit {
   }
 
   getSelecteDate = (event: MatDatepickerInputEvent<Date>) => {
-    let selected = new Date(event.value);
-    alert(selected.toISOString().slice(0,10).split('-').reverse().join('/') );
+    let departureDate = new Date(event.value);
+    // set the query string in URL
+    let start_date = (departureDate.getMonth() +1) +'/'+departureDate.getDate()+'/'+departureDate.getFullYear();//toISOString().slice(0,10).split('-').reverse().join('/');
+    this._router.navigate(['/hotels'], {
+      queryParams: {'start_date': start_date },
+      queryParamsHandling: 'merge'
+    });
   }
 
 }
